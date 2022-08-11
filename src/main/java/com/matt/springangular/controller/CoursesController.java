9a9b5589b80855144334469dfa8f5f1a0280c7fd
fabiovmat <fabio.meihua08@gmail.com>
2022-08-11 17:08:36 -0300

    Update CoursesController.java

--- src/main/java/com/matt/springangular/controller/CoursesController.java
+++ src/main/java/com/matt/springangular/controller/CoursesController.java
@@ -2,7 +2,12 @@ package com.matt.springangular.controller;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +31,13 @@ public class CoursesController {
     public @ResponseBody List<Course> list(){
 
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> create(@RequestBody Course course){
+        return ResponseEntity.status(HttpStatus.CREATED)
+        .body(courseRepository.save(course));
+        
     }
 
     
